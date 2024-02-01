1b5951789e14d6eb4bcb718bcd5da165bfef4a0c
Win Cheng <winzamark12@gmail.com>
2024-01-31 23:51:06 -0800

    Added CORS Methods

--- server/server.py
+++ server/server.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 @app.route("/", methods=['GET'])
 def return_home():
