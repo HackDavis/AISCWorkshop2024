4cca8e6054d61da9a31859552035a7bd40beb831
Win Cheng <winzamark12@gmail.com>
2024-01-31 23:48:55 -0800

    Changed CORS allowed

--- server/server.py
+++ server/server.py
@@ -5,15 +5,7 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
-CORS(app, resources={
-    r"/*": {
-        "origins": ["https://aisc-workshop2024-kten.vercel.app/", "https://aisc-workshop2024-kten-win-chengs-projects.vercel.app/",
-                     "http://localhost:3000"],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]
-    }
-})
-
+CORS(app)
 api = Api(app)
 
 
