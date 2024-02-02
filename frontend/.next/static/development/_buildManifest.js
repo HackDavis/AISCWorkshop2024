2c8f1643dbc9a8e42de3c6edaa96bdce35a63867
JayJ104 <123anjalijain@gmail.com>
2024-02-02 13:13:17 -0800

    deleted frontend/.next

--- frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/MLProjects":["static\u002Fchunks\u002Fpages\u002FMLProjects.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FMLProjects","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
