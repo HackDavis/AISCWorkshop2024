6986b0bc2d67e53245fdbef59e318d704ca5f79f
JayJ104 <123anjalijain@gmail.com>
2024-02-02 23:13:48 -0800

    models api fixed

--- frontend/pages/MLProjects/index.js
+++ frontend/pages/MLProjects/index.js
@@ -4,22 +4,51 @@ import Image from 'next/image';
 import Graph from './_components/graph';
 import Models from './_components/models';
 import Analysis from './_components/analysis';
+import { useState } from 'react';
 
 export default function MLProjects() {
+
+  const mlModels = ['logistic_regression', 'k_nearest_neighbors', 'support_vector_machine', 'decision_tree', 'random_forest', 'gradient_boosting', 'naive_bayes', 'neural_network', 'ada_boost', 'xg_boost']
+  const [checkedModels, setCheckedModels]  = useState([]);
+  const [chosenModels, setChosenModels] = useState([]);
+
+  const checklistHandler = (modelName) => {
+    if(checkedModels.includes(modelName)){
+      setCheckedModels((models) => models.filter((i) => i !== modelName));
+    } else{
+      setCheckedModels((models)=> [...models, modelName]);
+    }
+  }
+  const buttonHandler = (allModels) => {
+    setChosenModels(allModels);
+    setCheckedModels([]);
+  }
+
+
+  
   return (
     <div className={styles.container}>
       <div className={styles.container_inside}>
         <div className={styles.container_inside_window}>
           <div className={styles.container_inside_window_Card}>
-            <Graph />
+            {mlModels.map((model, index) => { 
+              return(
+                <div key={index}>
+                  <input id={model} type='checkbox' onChange={() => checklistHandler(model)}/>
+                  <label>{model}</label>
+                </div>)
+              })}
           </div>
-          <div className={styles.container_inside_window_Card} >
-            <Models />
+          <button onClick={() => buttonHandler(checkedModels)}>Run Models</button>
+          <div className={styles.container_inside_window_Card}>
+                <Graph />
           </div>
           <div className={styles.container_inside_window_Card}>
-            <Analysis />
+                <Analysis />          
           </div>
-          <div className={styles.container_inside_window_Card} />
+          <div className={styles.container_inside_window_Card}>
+            <Models props={chosenModels}/>
+          </div>
         </div>
         <div className={styles.container_inside_animals}>
           <Image src="/MLProjects/cow.svg" alt="cow" width={100} height={100} />
